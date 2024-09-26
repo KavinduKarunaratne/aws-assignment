8d2d60af339a3fcd07da749bfbb2fac05138888e
Kavindu Karunaratne <kavindu.kaumal@gmail.com>
2024-09-27 03:10:51 +0530

    Album song list

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import DisplayHome from "./DisplayHome";
+import DisplayAlbum from './DisplayAlbum';
 
 const Display = () => {
     return (
         <div className="w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0">
             <Router>
                 <Routes>
-                    <Route path='/' element={<DisplayHome/>} />
+                    <Route path='/' element={<DisplayHome />} />
+                    <Route path='/album/:id' element={<DisplayAlbum />} />
                 </Routes>
             </Router>
             
